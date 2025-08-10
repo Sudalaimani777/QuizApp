28c980e5a74a7a73142ee700e26fe712c9512e2f
Sudalaimani777 <sudalaimani5112007@gmail.com>
2025-08-10 22:19:36 +0530

    Removed the Unnecessary Lines of code that are not related to the real task

--- script.js
+++ script.js
@@ -1,86 +1,70 @@
 // TODO 1: Declare & assign variables pointing to the corresponding element(s)
-    // statement should be the "statement" div
-    // optionButtons should be all the elements within the "options" div
-    // explanation should be the "explanation" div
-    const statements = document.getElementById("statement");
-    const options = document.getElementById("options");
-    const explanations  = document.getElementById("explanation");
-  
-
-    // TODO 2: Declare & assign a variable called fact
-    // Its value should be an object with a statement, true/false answer, and explanation 
-    const facts = [
-        {
-            statement : "'1' + '1' === '2'",
-            isTrue : "",
-            isFalse : ""
-        },
-        {
-            statement : "",
-            isTrue : "",
-            isFalse : ""
-        },
-        {
-            statement : "",
-            isTrue : "",
-            isFalse : ""
-        },
-        {
-            statement : "",
-            isTrue : "",
-            isFalse : ""
-        },
-        {
-            statement : "",
-            isTrue : "",
-            isFalse : ""
-        }
-    ];
-
-    
-    // TODO 3: Set the text of the statement element to the fact's statement
-    statements.textContent = facts[0].statement;
-    
-     
-    // TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
-    // disable(button) should set the button element's attribute "disabled" to the value ""
-    // enable(button) should remove the attribute "disabled" from the button element
-     
+// statement should be the "statement" div
+// optionButtons should be all the elements within the "options" div
+// explanation should be the "explanation" div
+const statements = document.getElementById("statement");
+const options = document.getElementById("options");
+const explanations = document.getElementById("explanation");
 
 
-    // TODO 5: Declare an isCorrect function that compares a guess to the right answer
-    // isCorrect(guess) should return true if the guess matches the fact's answer
-    
+// TODO 2: Declare & assign a variable called fact
+// Its value should be an object with a statement, true/false answer, and explanation 
+const facts = [
+    {
+        statement: "'1' + '1' === '2'",
+        isTrue: "",
+        isFalse: ""
+    },
+    {
+        statement: "",
+        isTrue: "",
+        isFalse: ""
+    },
+    {
+        statement: "",
+        isTrue: "",
+        isFalse: ""
+    },
+    {
+        statement: "",
+        isTrue: "",
+        isFalse: ""
+    },
+    {
+        statement: "",
+        isTrue: "",
+        isFalse: ""
+    }
+];
 
 
-    // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-            // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
+// TODO 3: Set the text of the statement element to the fact's statement
+statements.textContent = facts[0].statement;
 
 
-            // TODO 7: Within the event handler function, 
-            // Use a for loop to disable all the option buttons
-
-
-            // TODO 8: Within the event handler function,
-            // Get the guessed value from the clicked button
-            // Use a conditional to compare the guess to the fact's answer
-            // and add the "correct"/"incorrect" class as appropriate
+// TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
+// disable(button) should set the button element's attribute "disabled" to the value ""
+// enable(button) should remove the attribute "disabled" from the button element
 
 
 
-const mul = (a, b) => console.log(a * b);
-// mul(5, 3)
+// TODO 5: Declare an isCorrect function that compares a guess to the right answer
+// isCorrect(guess) should return true if the guess matches the fact's answer
 
-const yell = (parameter) => {
-    const res = parameter.toUpperCase();
-    console.log(res);
-    return res;
-}
-// yell("mani");
 
-const longerThan = (valueOne, valueTwo) => {
-    valueOne = valueOne.length;
-    valueTwo = valueTwo.length;
-    return valueOne > valueTwo ? console.log("The 'Value One' array has a  longer length", valueOne) : console.log("The 'Value Two' array has a longer length", valueTwo)
-}
-longerThan([1, 2], [1, 2, 1])
+
+// TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
+// TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
+
+
+// TODO 7: Within the event handler function, 
+// Use a for loop to disable all the option buttons
+
+
+// TODO 8: Within the event handler function,
+// Get the guessed value from the clicked button
+// Use a conditional to compare the guess to the fact's answer
+// and add the "correct"/"incorrect" class as appropriate
+
+
+
